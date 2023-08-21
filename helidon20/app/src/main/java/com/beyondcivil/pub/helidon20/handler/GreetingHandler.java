6f41a51c8b93e988409e00a1cb6ad08f3cc7e048
Kira Johansson <kira@beyondcivil.com>
2023-08-21 11:27:37 -0600

    rm old stopwatch

--- helidon20/app/src/main/java/com/beyondcivil/pub/helidon20/handler/GreetingHandler.java
+++ helidon20/app/src/main/java/com/beyondcivil/pub/helidon20/handler/GreetingHandler.java
@@ -1,6 +1,5 @@
 package com.beyondcivil.pub.helidon20.handler;
 
-import com.google.common.base.Stopwatch;
 import io.helidon.nima.webserver.http.ServerRequest;
 import io.helidon.nima.webserver.http.ServerResponse;
 
@@ -8,7 +7,6 @@ public class GreetingHandler {
 
 
     public static void getWellDone(ServerRequest request, ServerResponse response) {
-        Stopwatch stopwatch = Stopwatch.createStarted();
         response.send("Well done!");
     }
 }
